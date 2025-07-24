928b85cc1688acba9b6fd2a58c8360a46a624420
Vo Quang Chien <voquangchien.dev@proton.me>
2025-07-24 20:56:12 +0700

    add adjacency-list implementation

---
+++ 1.representing-graph/adjacency-list.py
@@ -0,0 +1,103 @@
+from typing import Union
+
+
+class Edge:
+    def __init__(self, from_node: int, to_node: int, weight: float):
+        self.from_node = from_node
+        self.to_node = to_node
+        self.weight = weight
+
+
+class Node:
+    def __init__(self, index: int, label=None):
+        self.index = index
+        self.edges = {}
+        self.label = label
+
+    def num_edges(self) -> int:
+        return len(self.edges)
+
+    def get_edge(self, neightbor: int) -> Union[Edge, None]:
+        return self.edges.get(neightbor)
+
+    def add_edge(self, neighbor: int, weight: float):
+        self.edges[neighbor] = Edge(self.index, neighbor, weight)
+
+    def remove_edge(self, neighbor: int):
+        if neighbor in self.edges:
+            del self.edges[neighbor]
+
+    def get_edge_list(self) -> list:
+        return list(self.edges.values())
+
+    def get_sorted_edges_list(self) -> list:
+        result = []
+        neighbors = (list)(self.edges.keys())
+        neighbors.sort()
+
+        for n in neighbors:
+            result.append(self.edges[n])
+
+        return result
+
+
+class Graph:
+    def __init__(self, num_nodes: int, undirected: bool = False):
+        self.num_nodes = num_nodes
+        self.undirected = undirected
+        self.nodes = [Node(j) for j in range(num_nodes)]
+
+    def get_edge(self, from_node: int, to_node: int) -> Union[Edge, None]:
+        if from_node < 0 or from_node >= self.num_nodes:
+            raise IndexError
+        if to_node < 0 or to_node >= self.num_nodes:
+            raise IndexError
+        return self.nodes[from_node].get_edge(to_node)
+
+    def is_edge(self, from_node: int, to_node: int) -> bool:
+        return self.get_edge(from_node, to_node) is not None
+
+    def make_edge_list(self) -> list:
+        all_edges = []
+        for node in self.nodes:
+            for edge in node.edges.values():
+                all_edges.append(edge)
+        return all_edges
+
+    def insert_edge(self, from_node: int, to_node: int, weight: float):
+        if from_node < 0 or from_node >= self.num_nodes:
+            raise IndexError
+        if to_node < 0 or to_node >= self.num_nodes:
+            raise IndexError
+        self.nodes[from_node].add_edge(to_node, weight)
+        if self.undirected:
+            self.nodes[to_node].add_edge(from_node, weight)
+
+    def remove_edge(self, from_node: int, to_node: int):
+        if from_node < 0 or from_node >= self.num_nodes:
+            raise IndexError
+        if to_node < 0 or to_node >= self.num_nodes:
+            raise IndexError
+        self.nodes[from_node].remove_edge(to_node)
+        if self.undirected:
+            self.nodes[to_node].remove_edge(from_node)
+
+
+g = Graph(5, False)
+g.insert_edge(0, 1, 1.0)
+g.insert_edge(0, 3, 1.0)
+g.insert_edge(0, 4, 3.0)
+g.insert_edge(1, 2, 2.0)
+g.insert_edge(1, 4, 1.0)
+g.insert_edge(3, 4, 3.0)
+g.insert_edge(4, 2, 3.0)
+g.insert_edge(4, 3, 3.0)
+
+# Print the graph
+result = []
+for node in g.nodes:
+    to_nodes = []
+    for edge in node.edges.keys():
+        to_nodes.append(edge)
+    result.append(to_nodes)
+print(result)
