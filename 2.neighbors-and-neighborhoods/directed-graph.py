f86f48bd23c82699779cce477800c89429de2261
Vo Quang Chien <voquangchien.dev@proton.me>
2025-07-27 21:35:11 +0700

    update notes

--- 2.neighbors-and-neighborhoods/directed-graph.py
+++ 2.neighbors-and-neighborhoods/directed-graph.py
@@ -47,11 +47,28 @@ class Graph:
                 neighbors.add(node.index)
         return neighbors
 
+    def get_out_degree(self, index: int) -> int:
+        if index < 0 or index >= self.num_nodes:
+            raise IndexError
+        return len(self.nodes[index].edges)
+
+    def get_in_degree(self, index: int) -> int:
+        if index < 0 or index >= self.num_nodes:
+            raise IndexError
+        counter = 0
+        for node in self.nodes:
+            if index in node.edges:
+                counter += 1
+        return counter
+
 
 # The neighbors of a directed graph has two main types: in-neighbors and out-neighbors
 # The in-neighbors are all nodes that have edges with v as the destination. In other words,
 # the edge is incomming from node v's perspective.
 # The out-neighbors are all nodes to which v has an outgoing edge.
+
+# The degree of a graph is the number of times edges connect to a node
+# In directed graph, it has two types: in-degree and out-degree, and it similar to the neighbors concept
 g = Graph(6)
 
 g.insert_edge(0, 3, 1.0)
@@ -61,3 +78,7 @@ g.insert_edge(0, 1, 1.0)
 print(f"Out-neighbors of node 0 {g.get_out_neighbors(0)}")
 print(f"In-neighbors of node 0 {g.get_in_neighbors(0)}")
 print(f"In-neighbors of node 4 {g.get_in_neighbors(4)}")
+print(f"Out-degree of node 0 {g.get_out_degree(0)}")
+print(f"Out-degree of node 3 {g.get_out_degree(3)}")
+print(f"In-degree of node 3 {g.get_in_degree(3)}")
+print(f"In-degree of node 0 {g.get_in_degree(0)}")
