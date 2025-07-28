4c70256fac6d5164aa11ce214f7137c539e581fa
Vo Quang Chien <voquangchien.dev@proton.me>
2025-07-28 20:03:47 +0700

    update clustering coefficient note

--- 2.neighbors-and-neighborhoods/undirected-graph.py
+++ 2.neighbors-and-neighborhoods/undirected-graph.py
@@ -1,3 +1,6 @@
+from typing import List, Set
+
+
 # Minimal implementation of a adjacency-list representation graph
 class Edge:
     def __init__(self, from_node: int, to_node: int, weight: float):
@@ -13,6 +16,9 @@ class Node:
 
     def add_edge(self, to_node: int, weight: float):
         self.edges[to_node] = Edge(self.index, to_node, weight)
+
+    def get_edge_list(self) -> List[Edge]:
+        return list(self.edges.values())
 
 
 class Graph:
@@ -30,13 +36,27 @@ class Graph:
         if self.undirected:
             self.nodes[to_node].add_edge(from_node, weight)
 
-    def get_neighbors(self, index: int) -> set:
+    def get_neighbors(self, index: int) -> Set[int]:
         if index < 0 or index >= self.num_nodes:
             raise IndexError
         neighbors = set()
-        for edge in self.nodes[index].edges.values():
+        for edge in self.nodes[index].get_edge_list():
             neighbors.add(edge.to_node)
         return neighbors
+
+    # Local clustering coefficient
+    def clustering_coefficient(self, index: int) -> float:
+        neighbors = self.get_neighbors(index)
+        num_neighbors = len(neighbors)
+        count = 0
+        for n1 in neighbors:
+            for edge in self.nodes[n1].get_edge_list():
+                if edge.to_node > n1 and edge.to_node in neighbors:
+                    count += 1
+        total_possible = (num_neighbors * (num_neighbors - 1)) / 2.0
+        if total_possible == 0.0:
+            return 0.0
+        return count / total_possible
 
 
 g = Graph(6, True)  # 0 -> 5
@@ -55,3 +75,11 @@ g.insert_edge(5, 4, 1.0)
 # Print neighbors of each nodes
 for node in range(6):
     print(f"Neighbors of node {node} {g.get_neighbors(node)}")
+
+g2 = Graph(3, True)
+g2.insert_edge(0, 1, 1.0)
+g2.insert_edge(0, 2, 1.0)
+g2.insert_edge(1, 2, 1.0)
+print(
+    f"Clustering coefficient of node 0: {format(g2.clustering_coefficient(0), '.2f')}"
+)
